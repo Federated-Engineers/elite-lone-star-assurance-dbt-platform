068687794924a33e1c15d035742e2b20d868a50a
Chizobaeze <chizobaeze15@gmail.com>
2026-06-04 00:56:25 +0100

    fix: update CI pipeline and SQL model references

--- lone_star_dbt/models/my_first_dbt_model.sql
+++ lone_star_dbt/models/my_first_dbt_model.sql
@@ -1,5 +1,5 @@
 SELECT
-    id
-    , name
-    , age
-FROM elite_dbt_db.dbt_madebayo.my_first_dbt_model
+    id,
+    name,
+    age
+FROM {{ ref('my_first_dbt_model') }}
--- lone_star_dbt/models/my_second_dbt_model.sql
+++ lone_star_dbt/models/my_second_dbt_model.sql
@@ -1,2 +1,2 @@
 SELECT COUNT(age) AS id
-FROM elite_dbt_db.dbt_madebayo.my_first_dbt_model
+FROM {{ ref('my_first_dbt_model') }}
